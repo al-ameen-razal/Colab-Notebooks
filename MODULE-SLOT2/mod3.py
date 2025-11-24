74ab5b3aac26ee4537e100809b1ef04e52e33ca9
Al Ameen <waytoalameen8802@gmail.com>
2025-11-24 12:50:23 +0530

    Again learnt new, and better method.

--- MODULE-SLOT2/mod3.py
+++ MODULE-SLOT2/mod3.py
@@ -2,7 +2,14 @@
 
 # addition()
 
-from arithmetic import addition, difference
+# from arithmetic import addition, difference
+
+# addition()
+# difference(100, 20)
+
+from arithmetic import *
 
 addition()
-difference(100, 20)
+difference(50, 10)
+print(multiply())
+print(divisioin(100, 20))
