6ca7fb49675103d16d58c492d7d075e18198a302
Al Ameen <waytoalameen8802@gmail.com>
2025-11-24 12:36:16 +0530

    Learnt how to call userdefined funtion from another file.

---
+++ MODULE-SLOT2/mod1.py
@@ -0,0 +1,12 @@
+import arithmetic
+
+print(arithmetic.name)
+
+arithmetic.addition()
+
+arithmetic.difference(10, 5)
+
+print("Product = ", arithmetic.multiply())
+
+result = arithmetic.divisioin(50, 10)
+print("Division = ", result)
